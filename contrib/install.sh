137416afe549c69d7655c2b40cb13418ce44875b
yang <a0938079365@gmail.com>
2024-12-14 21:24:41 +0800

    fix action

--- contrib/install.sh
+++ contrib/install.sh
@@ -1,6 +1,49 @@
-cmakeargs=("-DCMAKE_INSTALL_PREFIX=${INSTALL_PREFIX}")
-cmakeargs+=("-DCMAKE_BUILD_TYPE=Release")
-cmakeargs+=("-DPYTHON_EXECUTABLE:FILEPATH=`which python3`")
-cmakeargs+=("-DPYBIND11_TEST=OFF")
-install ${PYBIND_ORG:-pybind} pybind11 ${PYBIND_BRANCH:-v2.7.1} \
+#!/bin/bash
+#
+# Copyright (C) 2018 Yung-Yu Chen <yyc@solvcon.net>.
+#
+# Build and install xtensor-python.
+
+INSTALL_PREFIX=${INSTALL_PREFIX:-/usr}
+INSTALL_VERSION=${INSTALL_VERSION:-master}
+
+install() {
+
+  githuborg=$1
+  pkgname=$2
+  pkgbranch=$3
+  pkgfull=$4
+  cmakeargs="${@:5}"
+  pkgrepo=https://github.com/$githuborg/$pkgname.git
+  repotar=https://github.com/$githuborg/$pkgname/archive/$pkgbranch.tar.gz
+
+  workdir=$(mktemp -d /tmp/build.XXXXXXXXX)
+  echo "Work directory: $workdir"
+  mkdir -p $workdir
+  pushd $workdir
+  echo "remote tarball: $repotar"
+  curl -sSL -o $pkgfull.tar.gz $repotar
+  rm -rf $pkgfull
+  tar xf $pkgfull.tar.gz
+  cd $pkgfull
+  mkdir -p build
+  cd build
+  cmake $cmakeargs ..
+  make install
+  popd
+  rm -rf $workdir
+
+}
+
+pybind11() {
+
+  cmakeargs=("-DCMAKE_INSTALL_PREFIX=${INSTALL_PREFIX}")
+  cmakeargs+=("-DCMAKE_BUILD_TYPE=Release")
+  cmakeargs+=("-DPYTHON_EXECUTABLE:FILEPATH=`which python3`")
+  cmakeargs+=("-DPYBIND11_TEST=OFF")
+  install ${PYBIND_ORG:-pybind} pybind11 ${PYBIND_BRANCH:-v2.7.1} \
     ${PYBIND_LOCAL:-pybind11-2.7.1} "${cmakeargs[@]}"
+
+}
+
+pybind11
